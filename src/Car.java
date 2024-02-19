559980a6e4daf13639f035644a2bb9e54e497a89
Dmitriy Stroganov <Distrog@gmail.com>
2024-02-19 21:13:51 +0700

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre(){
+        System.out.println("Меняем покрышку");
+    }
+    public void CheckEngine(){
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,22 @@
+public class ServiceStation {
+    public void check(Car car, Bicycle bicycle, Truck truck) {
+        if (car != null) {
+            System.out.println("Обслуживаем " + car.modelName);
+            for (int i = 0; i < car.wheelsCount; i++) {
+                car.updateTyre();
+            }
+            car.CheckEngine();
+        } else if(truck!=null){
+            System.out.println("Обслуживаем " + truck.modelName);
+            for (int i = 0; i < truck.wheelsCount; i++) {
+                truck.updateTyre();
+            }
+            truck.checkEngine();
+            truck.checkTrailer();
+        } else if (bicycle!=null){
+            for (int i = 0; i < bicycle.wheelsCount; i++) {
+                bicycle.updateTyre();
+            }
+        }
+    }
+}
